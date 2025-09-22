b9808a3462a2ed7fcf4fa007f32abfe1892a0c97
Astryr <santinojorgegullone@hotmail.com>
2025-09-22 16:57:29 -0300

    TP02 (Corrección)

--- Assets/Grupo 03/TP02/Ejercicio 02/Scripts/MyList.cs
+++ Assets/Grupo 03/TP02/Ejercicio 02/Scripts/MyList.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace MyLinkedList
 {
-    // Nodo de la lista
+
     public class MyNode<T>
     {
         public T Data;
@@ -27,7 +27,7 @@ namespace MyLinkedList
         }
     }
 
-    // Lista doblemente enlazada
+
     public class MyList<T>
     {
         private MyNode<T> root;
@@ -40,8 +40,7 @@ namespace MyLinkedList
             {
                 if (index < 0 || index >= Count) throw new IndexOutOfRangeException();
                 MyNode<T> current = root;
-                for (int i = 0; i < index; i++)
-                    current = current.Next;
+                for (int i = 0; i < index; i++) current = current.Next;
                 return current.Data;
             }
         }
@@ -124,6 +123,7 @@ namespace MyLinkedList
             if (index < 0 || index > Count) throw new IndexOutOfRangeException();
 
             MyNode<T> newNode = new MyNode<T>(value);
+
             if (index == Count)
             {
                 Add(value);
@@ -165,71 +165,6 @@ namespace MyLinkedList
             }
             result += "]";
             return result;
-        }
-
-        // BubbleSort
-        public void BubbleSort()
-        {
-            for (int i = 0; i < Count - 1; i++)
-            {
-                for (int j = 0; j < Count - i - 1; j++)
-                {
-                    if (((IComparable<T>)this[j]).CompareTo(this[j + 1]) > 0)
-                    {
-                        // Intercambiar valores
-                        T temp = this[j];
-                        SetAt(j, this[j + 1]);
-                        SetAt(j + 1, temp);
-                    }
-                }
-            }
-        }
-
-        // SelectionSort
-        public void SelectionSort()
-        {
-            for (int i = 0; i < Count - 1; i++)
-            {
-                int minIdx = i;
-                for (int j = i + 1; j < Count; j++)
-                {
-                    if (((IComparable<T>)this[j]).CompareTo(this[minIdx]) < 0)
-                    {
-                        minIdx = j;
-                    }
-                }
-                if (minIdx != i)
-                {
-                    T temp = this[i];
-                    SetAt(i, this[minIdx]);
-                    SetAt(minIdx, temp);
-                }
-            }
-        }
-
-        // InsertionSort
-        public void InsertionSort()
-        {
-            for (int i = 1; i < Count; i++)
-            {
-                T key = this[i];
-                int j = i - 1;
-                while (j >= 0 && ((IComparable<T>)this[j]).CompareTo(key) > 0)
-                {
-                    SetAt(j + 1, this[j]);
-                    j--;
-                }
-                SetAt(j + 1, key);
-            }
-        }
-
-        // Método auxiliar para modificar el valor en un índice
-        private void SetAt(int index, T value)
-        {
-            MyNode<T> current = root;
-            for (int i = 0; i < index; i++)
-                current = current.Next;
-            current.Data = value;
         }
     }
 }
---
+++ Assets/Grupo 03/TP02/Ejercicio 02/Scripts/TesterMyList.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using MyLinkedList;
+
+public class TesterMyList : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public TextMeshProUGUI displayText;
+    private MyList<string> myList;
+
+    private void Start()
+    {
+        myList = new MyList<string>();
+        UpdateDisplay();
+    }
+
+    public void AddItem()
+    {
+        if (!string.IsNullOrWhiteSpace(inputField.text))
+        {
+            myList.Add(inputField.text);
+            inputField.text = "";
+            UpdateDisplay();
+        }
+    }
+
+    public void AddRangeItems()
+    {
+        if (!string.IsNullOrWhiteSpace(inputField.text))
+        {
+            string[] parts = inputField.text.Split(',');
+            for (int i = 0; i < parts.Length; i++)
+                parts[i] = parts[i].Trim();
+            myList.AddRange(parts);
+            inputField.text = "";
+            UpdateDisplay();
+        }
+    }
+
+    public void RemoveItem()
+    {
+        if (!string.IsNullOrWhiteSpace(inputField.text))
+        {
+            bool removed = myList.Remove(inputField.text);
+            if (!removed)
+                Debug.Log("Elemento no encontrado en la lista.");
+            inputField.text = "";
+            UpdateDisplay();
+        }
+    }
+
+    public void RemoveAtIndex()
+    {
+        if (int.TryParse(inputField.text, out int index))
+        {
+            try
+            {
+                myList.RemoveAt(index);
+            }
+            catch
+            {
+                Debug.Log("Índice inválido.");
+            }
+            inputField.text = "";
+            UpdateDisplay();
+        }
+    }
+
+    public void InsertAtIndex()
+    {
+        string[] parts = inputField.text.Split(',');
+        if (parts.Length == 2 && int.TryParse(parts[0].Trim(), out int index))
+        {
+            string value = parts[1].Trim();
+            try
+            {
+                myList.Insert(index, value);
+            }
+            catch
+            {
+                Debug.Log("Índice inválido.");
+            }
+            inputField.text = "";
+            UpdateDisplay();
+        }
+    }
+
+    public void ClearList()
+    {
+        myList.Clear();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        displayText.text = $"Lista ({myList.Count}): {myList}";
+    }
+}
